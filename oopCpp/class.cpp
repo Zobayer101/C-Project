fdf72c43e1c6890b47e628e8a7d476dadd6514d2
Zobayer <mdzobayerhossen520@gmail.com>
2024-10-15 23:16:52 +0600

    nothing new

--- oopCpp/class.cpp
+++ oopCpp/class.cpp
@@ -18,8 +18,10 @@ class Car{
 int main(){
     Car object1("F1","Ferarir","blue",25);
     Car object2("XM","BMW","NaveBlue",5);
+    Car object3("v19","mastang","red",10);
     cout << object1.name<<'\t'<<object1.brand<<'\t'<<object1.color<<'\t'<<object1.age<<endl<<endl;
     cout << object2.name<<'\t'<<object2.brand<<'\t'<<object2.color<<'\t'<<object2.age;
+    cout << object3.name<<'\t'<<object3.brand<<'\t'<<object3.color<<'\t'<<object3.age;
 
     return 0;
 }
